5bec972e3c4b5259edea9f56a06728cdd7630bb6
jbped <pedigojacob@gmail.com>
2024-02-02 08:38:47 -0700

    change card to cards slice, iterate to print

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ func main() {
 	cards := []string{"Ace of Spades", newCard()}
 	fmt.Println(cards)
 	// iterate over the slice, and print
-	for i, card := range cards {
-		fmt.Println(i, card)
+	for _, card := range cards {
+		fmt.Println(card)
 	}
 }
 func newCard() string {
