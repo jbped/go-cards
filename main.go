74056db1c3507a1bb60e4801c59782b6427d9eb9
jbped <pedigojacob@gmail.com>
2024-02-02 08:36:48 -0700

    create card variable, print it

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import(
+	"fmt"
+)
+
 func main() {
-	
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades" // Preferred way to declare a variable
+	card := newCard()
+	fmt.Println(card)
+}
+func newCard() string {
+	return "Five of Diamonds"
 }
